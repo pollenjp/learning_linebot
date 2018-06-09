f8ac3ead7b5c835d9a836665c25bf3b0347043df
pollenjp <polleninjp@gmail.com>
2018-06-09 14:53:44 +0900

    [Update] index.js

--- index.js
+++ index.js
@@ -1,14 +1,56 @@
 // index.js
 
-const http = require("http");
+//------------------------------------------------------------
+//  Require
+//------------------------------------------------------------
+const express = require('express');
+const line = require('@line/bot-sdk');
+require('dotenv').load();               // require and load dotenv
 
-var server = http.createServer(
+//------------------------------------------------------------
+//  KEY
+//------------------------------------------------------------
+const config = {
+  channelAccessToken: process.env.MY_CHANNEL_ACCESS_TOKEN,  // .env
+  channelSecret:      process.env.MY_CHANNEL_SECRET         // .env
+}
+
+
+//------------------------------------------------------------
+//  app
+//------------------------------------------------------------
+const app = express();
+app.post("/webhook", line.middleware(config),
   function(req, res)
   {
-    res.end("Hello Node.js!");
-    console.log("Hello Node.js!");
+    Promis
+      .all(req.body.events.map(handleEvent))
+      .then(function(result){ res.json(result); });
   }
 );
 
-server.listen(process.env.PORT || 5000)
+
+const client = new line.Client(config);
+function handleEvent(event)
+{
+  if (event.type !== "message" || event.message.type !== "text"){
+    // ignore non-text-message event
+    return Promise.resolve(null);
+  }
+
+  // replay echo
+  const echo = { type: "text", text: event.message.text };
+  return client.replayMessage(event.replayToken, echo);
+}
+
+
+//------------------------------------------------------------
+//  Listen Port
+//------------------------------------------------------------
+const port = process.env.PORT || 3000;
+app.listen(port, function()
+  {
+    console.log("Listening on ${port}");
+  }
+);
 
