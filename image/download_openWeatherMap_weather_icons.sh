ba771488af16cf08fcb7fc81a16a89981c8abbdb
pollenjp <polleninjp@gmail.com>
2018-07-02 08:54:30 +0900

    [Add] image/

---
+++ image/download_openWeatherMap_weather_icons.sh
@@ -0,0 +1,29 @@
+wget http://openweathermap.org/img/w/01d.png
+wget http://openweathermap.org/img/w/02d.png
+wget http://openweathermap.org/img/w/03d.png
+wget http://openweathermap.org/img/w/04d.png
+wget http://openweathermap.org/img/w/09d.png
+wget http://openweathermap.org/img/w/10d.png
+wget http://openweathermap.org/img/w/11d.png
+wget http://openweathermap.org/img/w/13d.png
+wget http://openweathermap.org/img/w/50d.png
+
+#wget http://openweathermap.org/img/w/01d.png -O "clear sky.png"
+#wget http://openweathermap.org/img/w/02d.png -O "few clouds.png"
+#wget http://openweathermap.org/img/w/03d.png -O "scattered clouds.png"
+#wget http://openweathermap.org/img/w/04d.png -O "broken clouds.png"
+#wget http://openweathermap.org/img/w/09d.png -O "shower rain.png"
+#wget http://openweathermap.org/img/w/10d.png -O "rain.png"
+#wget http://openweathermap.org/img/w/11d.png -O "thunderstorm.png"
+#wget http://openweathermap.org/img/w/13d.png -O "snow.png"
+#wget http://openweathermap.org/img/w/50d.png -O "mist.png"
+
+#wget http://openweathermap.org/img/w/01d.png -O "clear_sky.png"
+#wget http://openweathermap.org/img/w/02d.png -O "few_clouds.png"
+#wget http://openweathermap.org/img/w/03d.png -O "scattered_clouds.png"
+#wget http://openweathermap.org/img/w/04d.png -O "broken_clouds.png"
+#wget http://openweathermap.org/img/w/09d.png -O "shower_rain.png"
+#wget http://openweathermap.org/img/w/10d.png -O "rain.png"
+#wget http://openweathermap.org/img/w/11d.png -O "thunderstorm.png"
+#wget http://openweathermap.org/img/w/13d.png -O "snow.png"
+#wget http://openweathermap.org/img/w/50d.png -O "mist.png"
